c3084f6e21ed7413463c0bf10a7159958edde83a
Illia Stetsenko <illiastetsenko@knu.ua>
2024-11-19 20:15:25 +0200

    BTH-15/SchemaBuilder: implemented methods for columns and relations

--- thorn_root/src/schema/schema_builder.rs
+++ thorn_root/src/schema/schema_builder.rs
@@ -6,6 +6,8 @@ use super::column::Column;
 use super::relationship::Relationship;
 use super::table::Table;
 
+// looks like we also need to find the way to save the SchemaBuilder in the file
+// oh God no, amazon ion
 pub struct SchemaBuilder<T>
 where
     T: DatabaseEngine,
@@ -47,13 +49,14 @@ where
         &mut self,
         table_name: &str,
         column: Column,
-        _save_with_error: bool,
-    ) -> Result<(), String> {
+        save_with_error: bool,
+    ) -> Result<&mut Table, String> {
         match self.tables.iter_mut().find(|t| t.name == table_name) {
-            Some(table) => {
-                table.add_column(column);
-                Ok(())
-            }
+            Some(table) => 
+                // save with error means if there is a column with such a name
+                // we would still save it but the migration will fail
+                table.add_column(column, save_with_error),
+            
             None => Err(format!("Table '{}' not found", table_name)),
         }
     }
@@ -61,58 +64,102 @@ where
     /// Updates a column in an existing table.
     pub fn update_column(
         &mut self,
-        _updated_column: Column,
-        _save_with_error: bool,
-    ) -> Result<(), String> {
-        todo!();
-        // Perform a validation if any relations to this entity has been added
-        // probably
+        table_name: &str,
+        updated_column: Column,
+        save_with_error: bool,
+    ) -> Result<&mut Table, String> {
+        match self.tables.iter_mut().find(|t| t.name == table_name) {
+            Some(table) => 
+                // save with error means if there is a column with such a name
+                // we would still save it but the migration will fail
+                table.update_column(updated_column, save_with_error),
+            
+            None => Err(format!("Table '{}' not found", table_name)),
+        }
     }
 
     /// Deletes a column from an existing table.
     pub fn delete_column(
         &mut self,
-        _updated_column: Column,
-        _save_with_error: bool,
-    ) -> Result<(), String> {
-        todo!();
-        // Perform a validation if any relations to this entity has been added
-        // probably
+        table_name: &str,
+        column_name_to_delete: &str,
+        save_with_error: bool,
+    ) -> Result<&mut Table, String> {
+        // to make it easier you firstly need to remove all the relations and then you can remove the column
+        match self.tables.iter_mut().find(|t| t.name == table_name) {
+            Some(table) => 
+                // save with error means if there is a column with such a name
+                // we would still save it but the migration will fail
+                table.delete_column(column_name_to_delete, save_with_error),
+            
+            None => Err(format!("Table '{}' not found", table_name)),
+        }
     }
 
     /// Add a relation to the schema.
     pub fn add_relation(
         &mut self,
-        _relationship: Relationship,
-        _save_with_error: bool,
+        relationship: Relationship,
+        save_with_error: bool,
     ) -> Result<(), String> {
-        todo!();
+        let from_table = match self.tables.iter_mut().find(|t| t.name == relationship.from_table) {
+            Some(table) => table,
+            None => return Err(format!("Table '{}' not found", relationship.from_table)),
+        };
+        from_table.add_relation(relationship.clone(), save_with_error)?;
+        let to_table: &mut Table = match self.tables.iter_mut().find(|t| t.name == relationship.to_table) {
+            Some(table) => table,
+            None => return Err(format!("Table '{}' not found", relationship.to_table)),
+        };
+        to_table.add_relation(relationship, save_with_error)?;
+        Ok(())   
     }
 
     /// Updates a relation in the schema.
     pub fn update_relation(
         &mut self,
-        _updated_relationship: Relationship,
-        _save_with_error: bool,
+        updated_relationship: Relationship,
+        save_with_error: bool,
     ) -> Result<(), String> {
-        todo!();
+        let from_table = match self.tables.iter_mut().find(|t| t.name == updated_relationship.from_table) {
+            Some(table) => table,
+            None => return Err(format!("Table '{}' not found", updated_relationship.from_table)),
+        };
+        from_table.update_relation(updated_relationship.clone(), save_with_error)?;
+        let to_table: &mut Table = match self.tables.iter_mut().find(|t| t.name == updated_relationship.to_table) {
+            Some(table) => table,
+            None => return Err(format!("Table '{}' not found", updated_relationship.to_table)),
+        };
+        to_table.update_relation(updated_relationship, save_with_error)?;
+        Ok(())
     }
 
     /// Deletes a relation from the schema.
     pub fn delete_relation(
         &mut self,
-        _updated_relationship: Relationship,
-        _save_with_error: bool,
+        relationship_to_delete: Relationship,
     ) -> Result<(), String> {
-        todo!();
+        let from_table = match self.tables.iter_mut().find(|t| t.name == relationship_to_delete.from_table) {
+            Some(table) => table,
+            None => return Err(format!("Table '{}' not found", relationship_to_delete.from_table)),
+        };
+        from_table.delete_relation(relationship_to_delete.clone())?;
+        let to_table: &mut Table = match self.tables.iter_mut().find(|t| t.name == relationship_to_delete.to_table) {
+            Some(table) => table,
+            None => return Err(format!("Table '{}' not found", relationship_to_delete.to_table)),
+        };
+        to_table.delete_relation(relationship_to_delete)?;
+        Ok(())
     }
 
     /// Builds the schema and returns the result.
-    pub fn build(self) -> Vec<Table> {
-        self.tables
+    pub fn build(&mut self) -> Result<(), String> {
+        // buildes the schema - saves it to the files
+        // self.tables
+        Ok(())
     }
 
     pub fn database_engine(&self) -> &T {
-        &self.database_engine
+        &self.database_engine // not sure but let it be here
     }
 }
--- thorn_root/src/schema/table.rs
+++ thorn_root/src/schema/table.rs
@@ -6,6 +6,7 @@ pub struct Table {
     pub name: String,
     pub columns: Vec<Column>,
     pub relationships: Vec<Relationship>,
+    pub saved_with_error: bool,
 }
 
 impl Table {
@@ -14,10 +15,37 @@ impl Table {
             name: table_name.to_string(),
             columns: vec![],
             relationships: vec![],
+            saved_with_error: false,
         }
     }
 
-    pub fn add_column(&mut self, _column: Column) {
-        todo!()
+    /// addes column into the table (TODO)
+    pub fn add_column(&mut self, _column: Column, _save_with_error: bool) -> Result<&mut Self, String> {
+        Ok(self)
+    }
+
+    /// updates the column in the table (TODO)
+    pub fn update_column(&mut self, _updated_column: Column, _save_with_error: bool) -> Result<&mut Self, String> {
+        Ok(self)
+    }
+
+    /// Deletes the column in the table (TODO)
+    pub fn delete_column(&mut self, _column_name_to_delete: &str, _save_with_error: bool) -> Result<&mut Self, String> {
+        Ok(self)
+    }
+
+    /// addes a relation into the table (TODO)
+    pub fn add_relation(&mut self, _relationship: Relationship, _save_with_error: bool) -> Result<&mut Self, String> {
+        Ok(self)
+    }
+
+    /// updated a relation in the table (TODO)
+    pub fn update_relation(&mut self, _updated_relationship: Relationship, _save_with_error: bool) -> Result<&mut Self, String> {
+        Ok(self)
+    }
+
+    /// Deletes a relation from the table (TODO)
+    pub fn delete_relation(&mut self, _relationship_to_delete: Relationship) -> Result<&mut Self, String> {
+        Ok(self)
     }
 }
