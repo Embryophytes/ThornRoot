42975bd8cef40ae92eee091b862946e0814366b2
Illia Stetsenko <illiastetsenko@knu.ua>
2024-11-25 21:43:51 +0200

    BTH-17/table: added manual PartialEq implementation for Relationhip due to ignore realtionship_type

--- thorn_root/src/schema/relationship.rs
+++ thorn_root/src/schema/relationship.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Eq)]
 pub struct Relationship {
     // api codes are used here
     pub from_table: String, // not sure about strings but we will see
@@ -12,6 +12,16 @@ pub struct Relationship {
     // If any - do nothing
     // If some from the previous migration haven't been found - delete
     // If new - create a new one
+}
+
+
+impl PartialEq for Relationship {
+    fn eq(&self, other: &Self) -> bool {
+        self.from_table == other.from_table
+            && self.from_column == other.from_column
+            && self.to_table == other.to_table
+            && self.to_column == other.to_column
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
