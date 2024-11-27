9527e9d9d000dff0b3cb97f0f9799690f831224a
Illia Stetsenko <illiastetsenko@knu.ua>
2024-11-27 09:23:30 +0200

    BTH-15/SchemaBuilder: cargo fmt fixes

--- thorn_root/src/schema/table.rs
+++ thorn_root/src/schema/table.rs
@@ -30,32 +30,55 @@ impl Table {
     }
 
     /// addes column into the table (TODO)
-    pub fn add_column(&mut self, _column: Column, _save_with_error: bool) -> Result<&mut Self, String> {
+    pub fn add_column(
+        &mut self,
+        _column: Column,
+        _save_with_error: bool,
+    ) -> Result<&mut Self, String> {
         Ok(self)
     }
 
     /// updates the column in the table (TODO)
-    pub fn update_column(&mut self, _updated_column: Column, _save_with_error: bool) -> Result<&mut Self, String> {
+    pub fn update_column(
+        &mut self,
+        _updated_column: Column,
+        _save_with_error: bool,
+    ) -> Result<&mut Self, String> {
         Ok(self)
     }
 
     /// Deletes the column in the table (TODO)
-    pub fn delete_column(&mut self, _column_name_to_delete: &str, _save_with_error: bool) -> Result<&mut Self, String> {
+    pub fn delete_column(
+        &mut self,
+        _column_name_to_delete: &str,
+        _save_with_error: bool,
+    ) -> Result<&mut Self, String> {
         Ok(self)
     }
 
     /// addes a relation into the table (TODO)
-    pub fn add_relation(&mut self, _relationship: Relationship, _save_with_error: bool) -> Result<&mut Self, String> {
+    pub fn add_relation(
+        &mut self,
+        _relationship: Relationship,
+        _save_with_error: bool,
+    ) -> Result<&mut Self, String> {
         Ok(self)
     }
 
     /// updated a relation in the table (TODO)
-    pub fn update_relation(&mut self, _updated_relationship: Relationship, _save_with_error: bool) -> Result<&mut Self, String> {
+    pub fn update_relation(
+        &mut self,
+        _updated_relationship: Relationship,
+        _save_with_error: bool,
+    ) -> Result<&mut Self, String> {
         Ok(self)
     }
 
     /// Deletes a relation from the table (TODO)
-    pub fn delete_relation(&mut self, _relationship_to_delete: Relationship) -> Result<&mut Self, String> {
+    pub fn delete_relation(
+        &mut self,
+        _relationship_to_delete: Relationship,
+    ) -> Result<&mut Self, String> {
         Ok(self)
     }
 }
