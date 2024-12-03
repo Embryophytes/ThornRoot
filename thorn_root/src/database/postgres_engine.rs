dd0016e47ebf9b31478783a0419a598ff74b67e0
Illia Stetsenko <illiastetsenko@knu.ua>
2024-12-03 19:07:27 +0200

    BTH-35/PostgresEngine: added a simple test

--- thorn_root/src/database/postgres_engine.rs
+++ thorn_root/src/database/postgres_engine.rs
@@ -15,3 +15,46 @@ impl DatabaseEngine for PostgresEngine {
         Ok(MigrationPlan::default())
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::schema::column::Column;
+    use crate::schema::data_type;
+
+    use super::*;
+    #[test]
+    fn test_2_tables_with_column_in_each() {
+        let mut schema = Schema::new();
+        schema.add_table("users").unwrap();
+        schema.add_table("orders").unwrap();
+
+        let users_table = schema.get_table_mut("users").unwrap();
+        users_table
+            .add_column(Column::new(
+                "id",
+                data_type::DataType::Integer,
+                true,
+                false,
+                false,
+            ))
+            .unwrap();
+
+        let orders_table = schema.get_table_mut("orders").unwrap();
+        orders_table
+            .add_column(Column::new(
+                "user_id",
+                data_type::DataType::Integer,
+                false,
+                true,
+                false,
+            ))
+            .unwrap();
+
+        let postgres_engine = PostgresEngine::default();
+        let res = postgres_engine.generate_migration_plan(&schema);
+        assert!(res.is_ok());
+    }
+
+}
+
